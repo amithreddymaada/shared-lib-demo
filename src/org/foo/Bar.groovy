c441db4eced6bfa9bd92a19e5ba4db9231fba1bf
Maada, AmithReddy <AmithReddy.Maada@experian.com>
2022-02-09 09:53:45 +0530

    Added static method inside Bar of src

--- src/org/foo/Bar.groovy
+++ src/org/foo/Bar.groovy
@@ -4,4 +4,8 @@ public class Bar{
     def sayHello(){
         println "Hello World from class 'Bar' of src"
     }
+
+    static void sayHelloStatic(){
+        println "Static hello world from class Bar of src"
+    }
 }
